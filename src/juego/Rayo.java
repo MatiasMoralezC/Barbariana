05df0c2a928af3975f1b2aa746e1e72d6e3ae820
Joel <joevecnasorb@gmail.com>
2021-10-15 21:10:51 -0300

    se agrego variable velocidad y metodo graficar de Rayo

--- src/juego/Rayo.java
+++ src/juego/Rayo.java
@@ -1,41 +1,54 @@
 package juego;
 
+import java.awt.Color;
+
+import entorno.Entorno;
+
 public class Rayo {
 	private int x;
 	private int y;
 	private int ancho;
 	private int alto;
+	private int velocidad;
 	private char direccion;
 
-	public Rayo(int x2, int y2, char mirada) {
-		// (x2,y2) posicion de raptor/vikinga dado que desde esa posicion inicia su
-		// movimiento el rayo
-		x = x2;
-		y = y2;
+	public Rayo(int xIn, int yIn, int velocidadIn, char direccionIn) {
+		// (xIn,yIn) es la posicion de raptor/vikinga dado que desde ahi se dispara el rayo
+		x = xIn;
+		y = yIn;
 		// ancho y alto determinados segun los pixeles de la imagen
 		ancho = 50;
-		alto = 50;
+		alto = 30;
+		// velocidad del rayo
+		velocidad = velocidadIn;
 		// direccion de la mirada de raptor/vikinga para determinar la direccion del rayo
-		direccion = mirada;
+		direccion = direccionIn;
 	}
 
 	public void mover() {
 		if (direccion == 'D')
-			x = x + 2;
+			x = x + velocidad;
 		else
-			x = x - 2;
+			x = x - velocidad;
 	}
 
 	public boolean fueraDePantalla() {
-		if (x < 0 || x > 800)
+		if (x < 0 || x > 800) // habrá que ajustar luego
 			return true;
 		return false;
 	}
 
-	public boolean procesarMovimiento() { 	// el metodo devuelve falso si esta fuera de pantalla
-		if (fueraDePantalla()) 				// ya que en ese caso no hay movimiento que procesar
+	public boolean procesarMovimiento() { // el metodo devuelve falso si esta fuera de pantalla
+		if (fueraDePantalla()) // ya que en ese caso no hay movimiento que procesar
 			return false;
 		mover();
 		return true;
+	}
+
+	public void graficar(Entorno e, char tipo) {
+		if (tipo == 'R')
+			e.dibujarRectangulo(x, y, ancho, alto, 0, Color.YELLOW); // rayo tipo relampago
+		else
+			e.dibujarRectangulo(x, y, ancho, alto, 0, Color.RED); // rayo tipo laser
 	}
 }
